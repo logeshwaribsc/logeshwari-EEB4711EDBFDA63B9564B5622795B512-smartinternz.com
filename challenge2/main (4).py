12f63e426ec8ad9019ab447e5b0b98481817ea0d
logeshwaribsc <144338320+logeshwaribsc@users.noreply.github.com>
2023-09-27 12:00:27 +0530

    Add files via upload

---
+++ challenge2/main (4).py
@@ -0,0 +1,14 @@
+class player:
+   def play(self):
+       play("The player is playing cricket.")
+class Batsman(player):
+  def play(self):
+      print("The batsman is batting.")
+class Bowler(player):
+  def play(sef):
+      print("The bowler is bowing.")
+batsman=Batsman()
+batsman=Bowler()
+
+batsman.play()
+batsman.play()
